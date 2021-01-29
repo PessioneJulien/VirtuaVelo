b92ad4c9742e2b78d0f52abc741be8f2f059298f
Pessione <julien.pessione83@gmail.com>
2021-01-29 14:19:04 +0100

    Initialisation du code PyQT

--- Julien/Main.py
+++ Julien/Main.py
@@ -1,2 +1,41 @@
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QInputDialog, QApplication, QLabel)
+import sys
+
+
+class Menu(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.width = 1920
+        self.height = 1080
+        self.StartBtn = QPushButton('Choisir un Parcours', self)
+        self.StatBtn = QPushButton('Statistiques', self)
+        self.ConfigBtn = QPushButton('Statistiques', self)
+        self.le = QLabel(self)
+        self.initUI()
+
+    def initUI(self):
+        # Add button                                                                                                     
+        self.StartBtn.setGeometry(round(self.width/10), round(self.height/5), self.width/3,round(self.height/7))
+
+        self.StartBtn.clicked.connect(self.showDialog)
+
+        # Add label
+        self.le.move(30, 62)
+        self.le.resize(400, 22)
+
+        self.setGeometry(0, 0, self.width, self.height)
+        self.setWindowTitle('Input dialog')
+        self.show()
+
+    def showDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter text:')
+        if ok:
+            self.le.setText(str(text))
+
+
 if __name__ == '__main__':
-    print("start")
+    app = QApplication(sys.argv)
+    screen = app.primaryScreen().size()
+    ex = Menu()
+    sys.exit(app.exec_())
--- Julien/VirtuaVelo.py
+++ Julien/VirtuaVelo.py
@@ -1,1 +1,43 @@
-import tkinter
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class Menu(QMainWindow):
+    def __init__(self):
+        super(Menu, self).__init__()
+        self.setGeometry(0, 0, 1920, 1080)
+        self.setWindowTitle("oui ma page")
+        self.label = QtWidgets.QLabel(self)
+        self.StartButton = QtWidgets.QPushButton(self)
+        self.initUI()
+
+    def initUI(self):
+        self.label.setText("wesh wesh")
+        self.label.move(50, 50)
+
+        self.StartButton.setText("le bouton")
+        self.StartButton.move(150, 150)
+        self.StartButton.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("You clicked the button")
+        self.Update()
+
+    def Update(self):
+        self.label.adjustSize()
+
+
+def window():
+    app = QApplication(sys.argv)
+    screen = app.primaryScreen()
+    print('Screen: %s' % screen.name())
+    size = screen.size()
+    print('Size: %d x %d' % (size.width(), size.height()))
+    win = Menu()
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    window()
